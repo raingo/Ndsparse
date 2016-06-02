9c75bc3c1fcc8d55f009a3c5f9f4b7c89bc6f27d
Raingo <raingomm@gmail.com>
2016-06-02 11:18:21 -0400

    include setup to use it as package

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+Code description
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+__author__ = "Raingo Lee (raingomm@gmail.com)"
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Ndsparse',
+      version='0.1',
+      description='Multi dimensional sparse matrix',
+      author='Raingo Lee',
+      author_email='raingomm at gmail.com',
+      url='https://github.com/raingo/Ndsparse',
+      packages=['Ndsparse'],
+     )
+
+# vim: tabstop=4 expandtab shiftwidth=2 softtabstop=2
