64df9b64e86a2e705ad208c976169fbf8dfe015b
Raingo <raingomm@gmail.com>
2016-06-01 22:35:46 -0400

    more operations supported

--- Ndsparse.py
+++ Ndsparse.py
@@ -1,12 +1,41 @@
+from collections import defaultdict
+import operator
+from functools import wraps
+import numpy as np
+
+SUPPORTED_DTYPE = (int, long, float, complex)
+def pairwise(fn):
+    @wraps(fn)
+    def wrapper(self, other):
+        # if other is scalar, only create a new one with the same nnz positions
+        # so the add and sub are different from regular
+        # !!! support broadcast
+        if isinstance(other, SUPPORTED_DTYPE):
+            keys = self.entries.keys()
+            entries = {key:other for key in keys}
+            other = self.__class__(entries)
+        assert len(self.shape) == len(other.shape), "shape mismatch"
+        lhs = self.repmat(other.shape)
+        rhs = other
+        if lhs is None:
+            lhs = other.repmat(self.shape)
+            rhs = self
+            assert lhs is not None, \
+                    "do not know how to broadcast: %s vs %s" \
+                    % (self.shape, other.shape)
+
+        return fn(lhs, rhs)
+    return wrapper
+
 class Ndsparse:
     """
     N-dimensional sparse matrix.
     entries: dict of positions and values in matrix
         key: N-tuple of positions (i,k,...)
         val: value at position
-    d: dimension
+    ndim: dimension
     """
-    
+
     def __init__(self, *args, **kwargs):
         """
         Constructor
@@ -15,11 +44,11 @@ class Ndsparse:
         NDsparse(nested list dense representation)
         """
         # NDsparse from a single scalar
-        if isinstance(args[0], (int, long, float, complex)):
+        if isinstance(args[0], SUPPORTED_DTYPE):
             self.entries = {():args[0]}
-            self.d = 0
             self.shape = ()
-        
+            self.dtype = type(args[0])
+
         # NDsparse from dict of pos,val pairs
         elif args[0].__class__.__name__ == 'dict':
             # Error handling:
@@ -27,38 +56,136 @@ class Ndsparse:
             # Make sure all indexes in keys are ints
             # Make sure all vals in dict are numbers
             self.entries = args[0]
-            self.d = len(self.entries.iterkeys().next())
             if len(args) > 1:
-                self.shape = args[1]
+                self.shape = tuple(args[1])
             else:
                 self.shape = getEntriesShape(args[0])
-    
+
         # NDsparse from list of lists (of lists...) dense format
         # 1st dim = rows, 2nd dim = cols, 3rd dim = pages, ...
         elif args[0].__class__.__name__ == 'list':
             self.entries = buildEntriesDictFromNestedLists(args[0])
-            self.d = len(self.entries.iterkeys().next())
             self.shape = getListsShape(args[0])
-                
+
         # Catch unsupported initialization
         else:
             raise Exception("Improper Ndsparse construction.")
-        
+
+        try:
+            key, value = self.entries.iteritems().next()
+        except:
+            value = 1.
+        self.dtype = type(value)
+
         # Cleanup
         self.removeZeros()
-    
+
+    @property
+    def ndim(self):
+        return len(self.shape)
+
+    def repmat(self, newshape):
+        diff_index = [(idx, s0, s1) for idx, (s0, s1)
+                in enumerate(zip(self.shape, newshape))
+                if s0 != s1]
+        if any([f[1] != 1 for f in diff_index]):
+            return None
+
+        prev = self.entries
+        for idx, _, s1 in diff_index:
+            res = {}
+            for key, value in prev.items():
+                key = list(key)
+                for i in range(s1):
+                    key[idx] = i
+                    res[tuple(key)] = value
+            prev = res
+        return self.__class__(prev)
+
     def copy(self):
         """
         Copy "constructor"
         """
         return Ndsparse(self.entries)
-        
+
+    @property
+    def size(self):
+        return reduce(operator.mul, self.shape, 1)
+
+    def swapaxes(self, A, B):
+        if A == B:
+            return
+
+        idxs = range(self.ndim)
+        idxs[A], idxs[B] = idxs[B], idxs[A]
+        self.transpose(idxs)
+
+    def ravel(self):
+        raise NotImplemented
+
+    def _reduce(self, axis, op):
+        full = range(self.ndim)
+        if axis is None:
+            axis = full
+
+        keep = sorted(list(set(full) - set(axis)))
+        def _sub(key):
+            return tuple([key[i] for i in keep])
+
+        mapping = defaultdict(list)
+        for key, value in self.entries.iteritems():
+            mapping[_sub(key)].append(value)
+
+        res = {}
+        for key, values in mapping.iteritems():
+            res[key] = op(values)
+
+        if len(keep) == 0:
+            return res[()]
+        else:
+            return self.__class__(res)
+
+    def sum(self, axis=None):
+        return self._reduce(axis=axis,op=sum)
+
+    def max(self, axis=None):
+        return self._reduce(axis=axis,op=max)
+
+    def __getitem__(self, key):
+        # if key[i] == None, all elements along that direction is returned
+        if len(key) > self.ndim:
+            # all key are None, equal to reshape
+            assert all([k is None for k in key[self.ndim:]]), \
+                    "invalid index: %s for ndim: %d" % (key, self.ndim)
+            self = self.copy()
+            self.reshape(self.shape+(1,)*(len(key)-self.ndim))
+
+        slices = [(idx, value) for idx, value in enumerate(key)
+                if value is not None]
+        if len(slices) == 0:
+            return self
+
+        assert all([self.shape[k] > v for k, v in slices]), \
+                'invalid index: %s for shape: %s' % (key, self.shape)
+        keep = sorted(list(set(range(self.ndim)) - set([f[0] for f in slices])))
+
+        def _map(key):
+            return tuple([key[f] for f in keep])
+
+        res = {}
+        new_shape = [self.shape[k] for k in keep]
+        for key, value in self.entries.items():
+            if all([key[k] == v for k, v in slices]):
+                # got one
+                res[_map(key)] = value
+        return self.__class__(res, new_shape)
+
     def __repr__(self):
         """
         String representation of Ndsparse class
         """
         rep = []
-        rep.append(''.join([str(self.d),'-d sparse tensor with ', str(self.nnz()), ' nonzero entries\n']))
+        rep.append(''.join([str(self.ndim),'-d sparse tensor with ', str(self.nnz()), ' nonzero entries\n']))
         poss = self.entries.keys()
         poss.sort()
         for pos in poss:
@@ -70,18 +197,18 @@ class Ndsparse:
         Number of nonzero entries. Number of indexed entries if no explicit 0's allowed.
         """
         return len(self.entries)
-        
+
     def addEntry(self,pos,val):
         # Error handling: make sure entry doesn't overwrite existing ones
         self.entries[pos] = val
-        
+
     def addEntries(self,newEntries):
         # Error handling: make sure entries don't overwrite existing ones
         self.entries.update(newEntries)
-        
+
     def mergePositions(self,other):
         """
-        Return (overlap, selfFree, otherFree) 
+        Return (overlap, selfFree, otherFree)
             overlap: set of tuples of positions where self and other overlap
             selfFree: set of tuples of positions where only self is nonzero
             otherFree: set of tuples of positions where only other is nonzero
@@ -92,7 +219,7 @@ class Ndsparse:
         selfFree = selfKeys.difference(otherKeys)
         otherFree = otherKeys.difference(selfKeys)
         return (overlap, selfFree, otherFree)
-    
+
     def removeZeros(self):
         """
         Remove explicit 0 entries in Ndsparse matrix
@@ -102,171 +229,81 @@ class Ndsparse:
             if val != 0:
                 newEntries[pos] = val
         self.entries = newEntries
-        
+
     def __eq__(self,other):
         """
         Test equality of 2 Ndsparse objects. Must have the same nonzero elements, rank, and dimensions.
         """
-        if self.d == other.d and self.shape == other.shape and self.entries == other.entries:
+        if self.ndim == other.ndim and self.shape == other.shape and self.entries == other.entries:
             return True
         else:
             return False
-        
+
+    @pairwise
     def __add__(self,other):
         """
         Elementwise addition of self + other.
         """
-        # Error handling: make sure Dims are same
         overlap, selfFree, otherFree = self.mergePositions(other)
         out = {}
-        
+
         for pos in overlap:
             out[pos] = self.entries[pos] + other.entries[pos]
         for pos in selfFree:
             out[pos] = self.entries[pos]
         for pos in otherFree:
             out[pos] = other.entries[pos]
-        
+
         return Ndsparse(out,self.shape)
-        
+
+    @pairwise
     def __sub__(self,other):
         """
         Elementwise subtraction of self - other.
         """
-        # Error handling: make sure Dims are same
         overlap, selfFree, otherFree = self.mergePositions(other)
         out = {}
-        
+
         for pos in overlap:
             out[pos] = self.entries[pos] - other.entries[pos]
         for pos in selfFree:
             out[pos] = self.entries[pos]
         for pos in otherFree:
             out[pos] = -other.entries[pos]
-        
+
         return Ndsparse(out,self.shape)
-    
+
+    @pairwise
     def __mul__(self,other):
         """
         Elementwise multiplication of self .* other.
         """
-        # Error handling: make sure Dims are same
         overlap, selfFree, otherFree = self.mergePositions(other)
         out = {}
-        
+
         for pos in overlap:
             out[pos] = self.entries[pos] * other.entries[pos]
-        
+
         return Ndsparse(out,self.shape)
-    
+
+    @pairwise
     def __div__(self,other):
         """
         Elementwise division of nonzero entries of self ./ other, casting ints to floats.
         """
-        # Error handling: make sure Dims are same
         overlap, selfFree, otherFree = self.mergePositions(other)
         out = {}
-        
+
         for pos in overlap:
             out[pos] = float(self.entries[pos]) / other.entries[pos]
-        
+
         return Ndsparse(out,self.shape)
-    
-    def matrixProduct(self,other):
-        """
-        Standard 2-d matrix multiply using ttt
-        """
-        assert self.d == 2 and other.d == 2, 'Matrices are wrong dimension for usual multiplication.'
-        return self.ttt(other,[1],[0])
-    
-    def outerProduct(self,other):
-        """
-        Outer product using ttt
-        """
-        return self.ttt(other)
-    
-    def kroneckerProduct(self,other):
-        """
-        Kronecker product of 2-d matrices using ttt and rearranging indices
-        """
-        assert self.d == 2 and other.d == 2, 'Matrices are wrong dimension for kronecker product.'
-        prod = self.ttt(other)
-        
-        # Dimensions: self(m x n) (x) other(p x q) = kprod(mp x nq)
-        m = self.shape[0]
-        n = self.shape[1]
-        p = other.shape[0]
-        q = other.shape[1]
-        
-        # Modify indices
-        kprod = {}
-        for pos,val in prod.entries.iteritems():
-            i = p*pos[0] + pos[2] + 1
-            j = q*pos[1] + pos[3] + 1
-            kprod[(i-1,j-1)] = val
-            
-        return Ndsparse(kprod,[m*p,n*q])
-    
-    def ttt(self,other,*args):
-        """
-        Tensor x tensor generalized multiplication. Should include inner and outer products and contraction.
-        Specify contraction dims with 1 list of dims in order for self followed by 1 list of dims in order for other
-        
-        Outer product: Contract on no dims
-        Inner product: Contract on all dims (specify order)
-        Product with contraction on arbitrary dimensions also
-        
-        Result index order: (selfIdxs\selfDim, otherIdxs\otherDim)
-        Time complexity: O(self.nnz * other.nnz)
-        """
-        
-        if len(args) == 0: # no contractions/outer product
-            selfDims = []
-            otherDims = []
-        elif len(args) == 2: # contractions specified
-            selfDims = args[0]
-            otherDims = args[1]
-            # Error handling: make sure Dims are present, contraction is proper, same lengths
-        else:
-            raise Exception("ttt requires 1 or 3 args.")
-            
-        # Accumulate nonzero positions 
-        terms = [] # list of tuples of (pos tuple, val) to sum
-        
-        for pos1,val1 in self.entries.iteritems():
-            
-            conIdx1s = [ item for i,item in enumerate(pos1) if i in selfDims ] # self's contracted indices
-            keepIdx1s = [ item for i,item in enumerate(pos1) if i not in selfDims ] # self's kept indices
-            
-            for pos2,val2 in other.entries.iteritems():
-                
-                conIdx2s = [ item for i,item in enumerate(pos2) if i in otherDims ] # other's contracted indices
-                keepIdx2s = [ item for i,item in enumerate(pos2) if i not in otherDims ] # other's kept indices
-                
-                pos = tuple(keepIdx1s + keepIdx2s)
-                
-                if conIdx1s == conIdx2s: # Match entries that share contraction index (including none)
-                    terms.append((pos, val1*val2))
-        
-        # Sum entries
-        out = {}
-        for entry in terms:
-            pos = entry[0]
-            val = entry[1]
-            if pos not in out:
-                out[pos] = val
-            else:
-                out[pos] += val
-        
-        shape = [ item for i,item in enumerate(self.shape) if i not in selfDims ] + \
-                [ item for i,item in enumerate(other.shape) if i not in otherDims ]
-        return Ndsparse(out,shape)
-    
+
     def transpose(self,permutation):
         """
         Transpose Ndsparse matrix in place
         permutation: tuple of new indices
-        Matrix starts out as (0,1,...,N) and can be transposed according to 
+        Matrix starts out as (0,1,...,N) and can be transposed according to
            the permutation (N,1,....0) or whatever, with N! possible permutations
         Note indexing starts at 0
         """
@@ -276,14 +313,25 @@ class Ndsparse:
         for key,value in self.entries.iteritems():
             out[permute(key,permutation)] = value
         self.entries = out
-        self.shape = list(permute(self.shape,permutation))
-    
-    def reshape(self,shapemat):
+        self.shape = tuple(permute(self.shape,permutation))
+
+    def reshape(self, shapemat):
         """
         Like the MATLAB reshape. http://www.mathworks.com/help/matlab/ref/reshape.html
         """
-        pass
-    
+        assert reduce(operator.mul, shapemat, 1) == self.size, \
+                'reshape can not change the number of elements'
+        def _map(key):
+            index = np.ravel_multi_index(key, self.shape)
+            return np.unravel_index(index, shapemat)
+
+        res = {}
+        for key, value in self.entries.items():
+            res[_map(key)] = value
+
+        self.entries = res
+        self.shape = tuple(shapemat)
+
 def permute(vec,permutation):
     """
     Permute vec tuple according to permutation tuple.
@@ -292,7 +340,7 @@ def permute(vec,permutation):
 
 def traverseWithIndices(superList, treeTypes=(list, tuple)):
     """
-    Traverse over tree structure (nested lists), with indices. Returns a nested list 
+    Traverse over tree structure (nested lists), with indices. Returns a nested list
     with the left element a nested list and the right element the next position.
     Call flatten to get into a nice form.
     """
@@ -305,7 +353,7 @@ def traverseWithIndices(superList, treeTypes=(list, tuple)):
                 yield [subValue,idxs]
     else:
         yield [superList,idxs]
-        
+
 def flatten(superList, treeTypes=(list, tuple)):
     '''
     Flatten arbitrarily nested lists into a single list, removing empty lists.
@@ -327,7 +375,7 @@ def buildEntriesDictFromNestedLists(nestedLists):
     #    Warning: first dim shouldn't be singleton
     if len(nestedLists) == 1:
         return {():nestedLists[0]}
-    
+
     entriesDict = {}
     for entry in traverseWithIndices(nestedLists):
         flatEntry = flatten(entry)
@@ -345,7 +393,7 @@ def getListsShape(nestedLists, treeTypes=(list, tuple)):
     while isinstance(lst, treeTypes):
         shape.append(len(lst))
         lst = lst[0]
-    return shape
+    return tuple(shape)
 
 def getEntriesShape(entries):
     """
@@ -356,33 +404,4 @@ def getEntriesShape(entries):
         for i,idx in enumerate(pos):
             if idx > maxIdxs[i]:
                 maxIdxs[i] = idx
-    return [idx+1 for idx in maxIdxs]
-
-# Testing code
-if __name__ == "__main__":
-    Al = [[[1,7,3], [2,8,4]], [[3,9,5], [4,0,6]], [[5,1,7], [6,2,8]], [[0,1,9], [1,0,3]]]
-    Bl = [[[5,1],[7,0],[8,4],[0,4]], [[0,3],[1,5],[9,6],[1,2]], [[4,9],[3,8],[6,7],[2,0]]]
-    print Al
-    print Bl
-    A = Ndsparse(Al)
-    B = Ndsparse(Bl)
-    print A
-    print B
-    print A.shape
-    print B.shape
-    #C = A.outerProduct(B)
-    #print C
-    #D = Ndsparse(6)
-    #print D
-    #E = A.outerProduct(D)
-    #print E
-    C = A.ttt(B,[0,1],[1,2])
-    #C = A.ttt(B)
-    #print C
-    print C.shape
-    Gl = [[1,2],[3,4]]
-    Hl = [[0,5],[6,7]]
-    G = Ndsparse(Gl)
-    H = Ndsparse(Hl)
-    print G.matrixProduct(G)
-    print G.kroneckerProduct(H)
+    return tuple([idx+1 for idx in maxIdxs])
---
+++ test_basic.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""
+Code description
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+__author__ = "Raingo Lee (raingomm@gmail.com)"
+
+import sys
+import os.path as osp
+
+from Ndsparse import Ndsparse
+
+def main():
+  data = {(0,0,0):1, (20,10,100):1}
+  mat = Ndsparse(data)
+  print(mat.dtype)
+  print(mat.size)
+  print(mat.ndim)
+
+  mat.swapaxes(0,2)
+  print(mat.shape)
+  mat.swapaxes(0,2)
+  print(mat.shape)
+
+  print(mat.sum())
+  print(mat.sum(axis=(1,2)))
+
+  print(mat.max())
+  print(mat.max(axis=(1,2)))
+
+  print(mat[3,4,2])
+
+  print(mat * 5)
+
+  # test broadcast
+  print(mat.shape)
+  mat2 = mat[None, None, None, None]
+  print(mat2.shape)
+  mat3 = mat2.repmat(mat.shape+(3,))
+  print(mat2.shape)
+  print(mat3.shape)
+  print(mat2 + mat3)
+  # test pairwise
+  # test slicing
+
+  pass
+
+if __name__ == "__main__":
+  main()
+
+# vim: tabstop=4 expandtab shiftwidth=2 softtabstop=2
